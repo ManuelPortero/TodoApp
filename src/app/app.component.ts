ab225c7ca1bb7fa7e1ab459fff15a30ca451f5ae
lolsax <manuel.portero.leiva@gmail.com>
2020-05-08 22:06:05 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
-}
+  title = 'TodoApp';
+  todos = [
+  { 
+    label: 'Do the shop',
+    done: false,
+    priority: 2
+  } ,
+  { 
+    label: 'Cut the grass',
+    done: false,
+    priority: 3
+  } ,
+  { 
+    label: 'Pay the council tax',
+    done: false,
+    priority: 1
+  } ,
+  { 
+    label: 'Go to the gym',
+    done: false,
+    priority: 2
+  } 
+  ];
+
+  addTodo(newTodoLabel){
+
+    var newTodo ={
+      label: newTodoLabel,
+      priority: 1,
+      done: false
+      };
+      this.todos.push(newTodo); 
+
+    }
+
+    deleteTodo(todo){
+      this.todos =this.todos.filter(t=>t.label !== todo.label);
+
+    }
+
+
+  }
